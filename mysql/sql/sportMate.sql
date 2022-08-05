23ad7b526c4c31206fef87ec2c61cb56825b37da
wjddnrud <dnwk203@naver.com>
2022-08-05 16:05:16 +0900

    UPDATE

--- mysql/sql/sportMate.sql
+++ mysql/sql/sportMate.sql
@@ -1,29 +1,31 @@
-USE biscoff;
+use biscoff;
 
--- 데이터를 입력하여 필요한 정보를 조회할수 있는 부분들을 찾아서 쿼리문 짜보기
-
--- ccg : ccg | cc : cc
--- 1 : 통신사 | 1 : skt
--- 1 : 통신사 | 2 : kt
--- 1 : 통신사 | 3 : lg
--- 미리 만들어 놓으면 유용함 만들어 보기
+-- CCG|CCG:CC|CCNAME
+-- 1 | 통신사 : 1 | SKT
+-- 1 | 통신사 : 2 | KT
+-- 1 | 통신사 : 3 | LGT
+-- 나오게 쿼리 짜놓기
 
 SELECT
 	a.seq
     ,a.ccg_name
-    ,b.cc_name
     ,b.cc_key
+    ,b.cc_name
 FROM ccg a
-JOIN cc b on b.ccg_seq = a.seq
-WHERE a.seq = "7"
-;
-
--- 로그인 부분
-
--- 코멘트 부분
-
--- 구매 부분
+left join cc b on b.ccg_seq = a.seq;
+-- WHERE 
+-- a.seq="7";
+    
 
 
+-- 로그인
+-- 로그인은 간단해서 a 넣지 않았지만 다른것들은 넣어서 해보기
+-- SELECT * FROM member WHERE id = "" and password = "";
 
+-- 어쩌구 목록
 
+-- 코멘트 목록
+
+-- 극장 목록
+
+-- 데이터를 입력해서 필요한 정보를 조회 할때 어떤게 필요할지를 생각해보고 쿼리문 짜기
