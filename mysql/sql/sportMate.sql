c3da9adab9739366d2f35207b3163ec6f218bb7d
wjddnrud <dnwk203@naver.com>
2022-08-16 01:51:18 +0900

    update

--- mysql/sql/sportMate.sql
+++ mysql/sql/sportMate.sql
@@ -1,6 +1,8 @@
 -- 작성시 유의사항!!!!
 -- 1. *아스테릭 사용금지
 -- 2. 실제화면에 보이는데로 sql 작성
+-- 데이터를 입력해서 필요한 정보를 조회 할때 어떤게 필요할지를 생각해보고 쿼리문 짜기
+
 
 use biscoff;
 
@@ -15,26 +17,44 @@ left join cc b on b.ccg_seq = a.seq;
 -- WHERE 
 -- 	a.ccg_name="통신사";
 
--- 로그인은 간단해서 a 넣지 않았지만 다른것들은 넣어서 해보기
+-- 로그인
 SELECT 
-	* 
-FROM member 
-WHERE 
-	id = "" 
-    and password = ""
+	id
+    ,name
+FROM 
+	member 
+WHERE 1=1
+	and id = "dnwk101" 
+	and password = "1234567a"
 ; 
 
--- 데이터를 입력해서 필요한 정보를 조회 할때 어떤게 필요할지를 생각해보고 쿼리문 짜기
+
 
 
 -- 로그인
 SELECT
-	id
+	seq
+	,id
     ,password
+    ,name
+    ,dob
+    ,gender
+    ,job
+    ,address
+    ,phoneNumber
+    ,mobileCarrier
+    ,way_to_regist
+    ,address_detail
+    ,zipcode
 FROM member
 -- WHERE
 -- 	seq = 2
 ;
+
+
+-- 메인화면(카테고리별 게시물 총 개수(total) 및 하루 신규글 갯수(new)
+
+
 
 -- 회원 정보 확인 및 수정
 SELECT
@@ -69,8 +89,8 @@ SELECT
 	seq
     ,writer
     ,title
-    ,creat_date
     ,contents
+    ,creat_date
     ,viewCount
 FROM post
 WHERE seq = 1
@@ -84,9 +104,10 @@ SELECT
     ,contents
     ,post_seq
 FROM comment
+WHERE post_seq = 1
 ;
 
--- 게시물 상세.댓글.대댓글
+-- 대댓글
 SELECT
 	seq
     ,create_date
@@ -94,6 +115,7 @@ SELECT
     ,contents
     ,comment_seq
 FROM recomment
+WHERE seq = 8
 ;
 
 -- 게시물 + 댓글 + 대댓글 + 카테고리  (분류 버튼이나 카테고리 분류를 했을때 이렇게 쿼리를 작성해서 필요한것만 뽑아서 볼수 있게 쿼리문 작성)
@@ -104,16 +126,6 @@ SELECT
     ,a.creat_date
     ,a.contents
     ,a.viewCount
-    ,b.seq
-    ,b.create_date
-    ,b.writer
-    ,b.contents
-    ,b.post_seq
-	,c.seq
-	,c.create_date
-	,c.writer
-	,c.contents
-	,c.comment_seq
 	,d.seq
 	,d.category
 	,d.post_seq
@@ -122,6 +134,18 @@ join comment b on b.post_seq = a.seq
 join recomment c on c.comment_seq = b.seq
 join category d on d.post_seq = a.seq;
 
--- 그룹 
-
+-- 운동 그룹 리스트
+SELECT
+	seq
+    ,creator
+    ,group_name
+    ,sports
+    ,location
+    ,people_number
+    ,startTime
+    ,endTime
+    ,detail
+    ,map
+FROM sportsGroup
+;
 
