25b545081cfe12509dbe2b8db7cbf794f5315e48
wjddnrud <dnwk203@naver.com>
2022-08-19 00:38:48 +0900

    update

---
+++ mysql/sql/test.sql
@@ -0,0 +1,23 @@
+use biscoff;
+
+
+insert into uploaded(
+	type
+	,defaultNy
+    ,sort
+    ,originalName
+    ,uuidName
+    ,ext
+    ,size
+    ,delNy
+    ,pseq
+)
+
+values(
+	"image"
+    ,0
+    ,"image"
+)
+;
+
+select * from uploaded;
